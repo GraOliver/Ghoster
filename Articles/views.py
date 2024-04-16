69586b9cad6d2ef3b066a2a24e5197a7d65588a0
bygracenn@github.com <bygracenn@github.com>
2024-04-16 16:56:03 +0200

    dernier commit

--- Articles/views.py
+++ Articles/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.views.generic import View
 
 
-templates ="Articles/index.html"
+templates ="Articles/base.html"
 def index(request):
     return render(request,templates)
     
