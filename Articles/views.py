ab0f49ff376fd2a5d2545d10b6f67368f57121a1
bygracenn@github.com <bygracenn@github.com>
2024-04-29 12:08:15 +0200

    modelisation blog

--- Articles/views.py
+++ Articles/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render,redirect
 from django.views.generic import View
-
+from django.template import loader
+from django.contrib.auth.decorators import login_required
+from . import models,forms
 
 templates ="Articles/base.html"
 def index(request):
     return render(request,templates)
     
+class UploadPhotoVente(View):
+    @login_required # gestion des utillisateur connecter
+    def get(self,request):
+        pass
+    @login_required # gestion des utillisateur connecter
+    def post(self,request):
+        pass
+    pass
+
+class Blog(View):
+    def get(self,request):
+        pass
+    def post(self,request):
+        pass
+    pass
